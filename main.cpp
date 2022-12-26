be72cab67c7e02b4d15844cf9a23aec0b2bfc419
QD Studio <78663552+Kolyah35@users.noreply.github.com>
2022-12-26 22:23:22 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,3 @@
-/*******************************************************************************************
-*
-*   raylib-cpp [core] example - Basic window (adapted for HTML5 platform)
-*
-*   This example is prepared to compile for PLATFORM_WEB, PLATFORM_DESKTOP and PLATFORM_RPI
-*   As you will notice, code structure is slightly diferent to the other examples...
-*   To compile it for PLATFORM_WEB just uncomment #define PLATFORM_WEB at beginning
-*
-*   This example has been created using raylib-cpp (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2015 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "includes.h"
 #include "Player.h"
 #include "Laser.h"
@@ -39,10 +24,6 @@ bool showhitbox = false; // Показывать хитбоксы?
 Color bgcolor = DARKGREEN; // Цвет бг
 vector<Laser*> lasers; // Массив из лазеров
 vector<Enemy> enemys; // Массив из врагов
-
-//----------------------------------------------------------------------------------
-// Module Functions Declaration
-//----------------------------------------------------------------------------------
 
 //----------------------------------------------------------------------------------
 // Main Enry Point
